a186b1ceeb77fabc8cc99f4eac206efdd5659a6c
ANGELICA GARCES <abgs15@gmail.com>
2019-02-10 00:49:26 -0500

    Add files via upload

---
+++ server/models/roles.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const RolesSchema = new Schema({
+    name: { type: String, required: true},
+    id_user: { type: String, required: true},
+  
+});
+
+module.exports = mongoose.model('Roles', RolesSchema);
