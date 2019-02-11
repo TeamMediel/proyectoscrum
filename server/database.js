c3026b1c2dc084d81bddb768eecaf27cc0053291
LissetteHuilca <lissette.huilca@gmail.com>
2019-02-11 02:10:28 -0500

    cambios

--- server/database.js
+++ server/database.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 
 const URI = 'mongodb://localhost/crud-midi';
 
-mongoose.connect(URI)
+mongoose.connect(URI, { useNewUrlParser: true })
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(err));
 
+
+   
 module.exports = mongoose;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require ('express');
 const morgan = require('morgan');
-//const cors = require('cors');
+const cors = require('cors');
 const app = express();
 
 const { mongoose } = require('./database');
@@ -11,10 +11,12 @@ app.set('port', process.env.PORT || 3000);
 //Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
-//app.use(cors({origin: 'http://localhost:4200'}));
+app.use(cors({origin: 'http://localhost:4200'}));
 
 //Routes
 app.use('/user',require('./routes/user.routes'));
+app.use('/roles',require('./routes/roles.routes'));
+app.use('/auth',require('./routes/auth.routes'));
 
 //Starting the server
 app.listen(app.get('port'), () => {
