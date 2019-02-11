be41af9cc64ed5a80bf919d458a21487ddbb514a
LissetteHuilca <lissette.huilca@gmail.com>
2019-02-11 02:11:54 -0500

    routes

--- server/routes/roles.routes.js
+++ server/routes/roles.routes.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const roles = require('../controllers/roles.controller');
 
 router.get('/', roles.getRoles);
-router.post('/', roles.createRoles);
+router.post('/', roles.createRol);
 router.get('/:id', roles.getRol);
-router.put('/:id', roles.editRoles);
-router.delete('/:id', roles.deleteRoles);
+router.put('/:id', roles.editRol);
+router.delete('/:id', roles.deleteRol);
 
 module.exports = router;
