4a26be5b4baf7d2adea8cf41ab40e93810c53337
LissetteHuilca <lissette.huilca@gmail.com>
2019-02-11 02:22:09 -0500

    app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,19 @@ import { ContactoComponent } from './components/contacto/contacto.component';
 import { NosotrosComponent } from './components/nosotros/nosotros.component';
 import { PoliticasComponent } from './components/politicas/politicas.component';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { RegisterComponent } from './components/user/register/register.component';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
+import { RolesComponent } from './components/roles/roles.component';
+import { JuegosComponent } from './components/juegos/juegos.component';
+
 
 export const routes: Routes = [
+  {path: 'juegos', component: JuegosComponent},
+  {path: 'roles', component: RolesComponent},
+  {path: 'usuarios', component: UsuariosComponent},
+  {path: 'user/register', component: RegisterComponent},
+  {path: 'user/login', component: LoginComponent},
   {path: 'productos', component: ProductosComponent},
   {path: 'nosotros', component: NosotrosComponent},
   {path: 'politicas', component: PoliticasComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductosComponent } from './components/productos/productos.component';
@@ -8,6 +11,12 @@ import { ContactoComponent } from './components/contacto/contacto.component';
 import { NosotrosComponent } from './components/nosotros/nosotros.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { PoliticasComponent } from './components/politicas/politicas.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { RegisterComponent } from './components/user/register/register.component';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
+import { RolesComponent } from './components/roles/roles.component';
+import { JuegosComponent } from './components/juegos/juegos.component';
+
 
 @NgModule({
   declarations: [
@@ -16,11 +25,19 @@ import { PoliticasComponent } from './components/politicas/politicas.component';
     ContactoComponent,
     NosotrosComponent,
     InicioComponent,
-    PoliticasComponent
+    PoliticasComponent,
+    LoginComponent,
+    RegisterComponent,
+    UsuariosComponent,
+    RolesComponent,
+    JuegosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
