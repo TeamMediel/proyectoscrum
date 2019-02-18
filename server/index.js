7cd88521a301f80133ed24caf453dbaeaafa25fe
GiPaolaOrtiz <paogi_ortiz@hotmail.com>
2019-02-18 01:34:15 -0500

    Formulario de Contacto

--- server/index.js
+++ server/index.js
@@ -2,8 +2,25 @@ const express = require ('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
 
 const { mongoose } = require('./database');
+
+const transporter = nodemailer.createTransport({
+
+    host: 'smtp.gmail.com',
+    provider: 'gmail',
+    port: 465,
+    secure: true,
+    auth: {
+        user: 'kimmym1107@gmail.com', // Enter here email address from which you want to send emails
+        pass: 'russofyrayito' // Enter here password for email account from which you want to send emails
+    },
+    tls: {
+    rejectUnauthorized: false
+    }
+});
 
 //Settings
 app.set('port', process.env.PORT || 3000);
@@ -11,7 +28,14 @@ app.set('port', process.env.PORT || 3000);
 //Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(bodyParser.json());
 app.use(cors({origin: 'http://localhost:4200'}));
+app.use(function (req, res, next) {
+
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 //Routes
 app.use('/user',require('./routes/user.routes'));
@@ -22,3 +46,67 @@ app.use('/lineagrafica',require('./routes/lineagrafica.routes'));
 app.listen(app.get('port'), () => {
     console.log('Server on port 3000');
 });
+app.post('/send', function (req, res) {
+
+    let senderName = req.body.contactFormName;
+    let senderEmail = req.body.contactFormEmail;
+    let messageSubject = req.body.contactFormSubjects;
+    let messageText = req.body.contactFormMessage;
+    let copyToSender = req.body.contactFormCopy;
+  
+    let mailOptions = {
+      to: ['ginpaort@espol.edu.ec','rlhuilca@espol.edu.ec','jpvalle@espol.edu.ec','kipamuno@espol.edu.ec'], // Enter here the email address on which you want to send emails from your customers
+      from: senderName,
+      subject: messageSubject,
+      text: messageText,
+      replyTo: senderEmail
+
+    };
+  
+    if (senderName === '') {
+      res.status(400);
+      res.send({
+      message: 'Bad request'
+      });
+      return;
+    }
+  
+    if (senderEmail === '') {
+      res.status(400);
+      res.send({
+      message: 'Bad request'
+      });
+      return;
+    }
+  
+    if (messageSubject === '') {
+      res.status(400);
+      res.send({
+      message: 'Bad request'
+      });
+      return;
+    }
+  
+    if (messageText === '') {
+      res.status(400);
+      res.send({
+      message: 'Bad request'
+      });
+      return;
+    }
+  
+    if (copyToSender) {
+      mailOptions.to.push(senderEmail);
+    }
+  
+    transporter.sendMail(mailOptions, function (error, response) {
+      if (error) {
+        console.log(error);
+        res.end('error');
+      } else {
+        console.log('Message sent: ', response);
+        res.end('sent');
+      }
+    });
+  });
+  
--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ConexionService } from '../../services/conexion.service';
+import { NgForm } from '@angular/forms';
+import { Contacto } from 'src/app/models/contacto';
+  
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
   styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent implements OnInit {
+  private contacto:Contacto;
+  optionsSelect: Array<any>;
+  ngOnInit(){
+    this.contacto = new Contacto();
+  }
 
-  constructor() { }
+  constructor(private connectionService: ConexionService) {
 
-  ngOnInit() {
+  }
+
+  enviarContacto(form: NgForm) {
+    
+    this.connectionService.sendMessage(form.value).subscribe(() => {
+      console.log(form.value);
+      alert('Your message has been sent.');
+      form.reset();
+    }, error => {
+      console.log('Error', error);
+    });
   }
 
 }
