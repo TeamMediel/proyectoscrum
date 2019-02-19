574762bf8114b13567aea6fdfbb03daa8994e754
kehylaicaza <keilacicaza@gmail.com>
2019-02-19 13:02:35 -0500

    service encuesta

--- src/app/components/encuesta/encuesta.component.ts
+++ src/app/components/encuesta/encuesta.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { EncuestaService } from '../../services/encuesta.service';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Encuesta } from 'src/app/models/encuesta';
 
+declare var M: any;
 @Component({
   selector: 'app-encuesta',
   templateUrl: './encuesta.component.html',
-  styleUrls: ['./encuesta.component.css']
+  styleUrls: ['./encuesta.component.css'],
+  providers: [EncuestaService]
 })
 export class EncuestaComponent implements OnInit {
-
-  constructor() { }
+  
+  constructor(private encuestaService: EncuestaService) { }
 
   ngOnInit() {
   }
+
+  addEncuesta(form: NgForm) {
+  
+    this.encuestaService.postEncuesta(form.value)
+      .subscribe(res => {
+        M.toast({html: 'Enviado'});
+     
+      });
+    }
+  
+ 
 
 }
