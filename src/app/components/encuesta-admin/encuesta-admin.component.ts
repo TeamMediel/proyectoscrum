f83776189455cc05ce91b47eb9e830a98f32b78f
kehylaicaza <keilacicaza@gmail.com>
2019-02-20 00:07:37 -0500

    agg usuario a encuesta

---
+++ src/app/components/encuesta-admin/encuesta-admin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-encuesta-admin',
+  templateUrl: './encuesta-admin.component.html',
+  styleUrls: ['./encuesta-admin.component.css']
+})
+export class EncuestaAdminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
